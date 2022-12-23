c958ecc041bed1f8afdc4c0d2048c7ee6a0b70f1
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-12-22 21:41:34 -0300

    :sparkles: feat: game items created

---
+++ src/data/items.ts
@@ -0,0 +1,14 @@
+import android from '../svgs/android.svg';
+import truck from '../svgs/truck.svg';
+import disney from '../svgs/disney.svg';
+import star from '../svgs/star.svg';
+import gasoline from '../svgs/gasoline.svg';
+import motorcycle from '../assets/motorcycle.svg';
+export const items = [
+  { name: 'android', icon: android },
+  { name: 'truck', icon: truck },
+  { name: 'disney', icon: disney },
+  { name: 'star', icon: star },
+  { name: 'gasoline', icon: gasoline },
+  { name: 'motorcycle', icon: motorcycle },
+];
